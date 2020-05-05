7ec200e01a3ea1da2a4d9d0d431ca766b5cdbff9
Ramprit Sahani <21260321+Ramprits@users.noreply.github.com>
2020-05-05 22:27:14 +0530

    Finishing menu item style

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -4,13 +4,16 @@ import './menu-item.styles.scss';
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
     <Fragment>
-      <div
-        className={`${size} menu-item`}
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      >
+      <div className={`${size} menu-item`}>
+        <div
+          className="background-image"
+          style={{
+            backgroundImage: `url(${imageUrl})`
+          }}
+        />
         <div className="content">
-          <h1 className="title">{title}</h1>
-          <span className="subtitle">Shop Now</span>
+          <h1 className="title">{title.toUpperCase()}</h1>
+          <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
     </Fragment>
