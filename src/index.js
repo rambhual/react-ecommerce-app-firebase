13a5255d7521b5158a144780ecae29205258bf80
Ramprit Sahani <21260321+Ramprits@users.noreply.github.com>
2020-05-05 19:03:52 +0530

    Adding router package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import App from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   rootElement
 );
